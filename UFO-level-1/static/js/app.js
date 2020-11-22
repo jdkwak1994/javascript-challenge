e806cd0f93ecfffe7420ae60829f3ea23b8713fb
jdkwak1994 <jdkwak1994@gmail.com>
2020-11-21 22:13:27 -0500

    worked on some part

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,17 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputdate = d3.select("#datetime");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"];
+
+button.on("click", filterdisplay);
+
+function filterdisplay() {
+    d3.event.preventDefault();
+	var searchdate = inputdate.property("value").trim();
+    var filteredData = data.filter(data => data.datetime === searchdate);
+    console.log(searchdate);
+	showtable(filteredData);
+};
